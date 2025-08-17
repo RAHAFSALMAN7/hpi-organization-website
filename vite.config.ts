2440bd6afbb3af8d650fc67201eebe4181d03f5c
s <s12027798@stu.najah.edu>
2025-08-17 14:39:22 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+    force: true,
+    esbuildOptions: {
+      logLevel: 'silent', 
+    },
   },
-  base: './', // ← هذا السطر مهم للنشر على Netlify
-});
+  base: './', // ← مهم للنشر على Netlify
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (
+          warning.code === 'UNUSED_EXTERNAL_IMPORT' ||
+          warning.code === 'UNRESOLVED_IMPORT' ||
+          warning.code === 'MISSING_EXPORT' ||
+          warning.code === 'CIRCULAR_DEPENDENCY'
+        ) {
+          return
+        }
+        warn(warning)
+      },
+    },
+  },
+})
